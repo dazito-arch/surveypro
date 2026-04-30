c762836c2d215b3ec9bec7356d5ff4cee8b5fa1d
Diego Zelaya <dzelaya@PLASTIFORTE.COM>
2026-04-30 14:07:12 -0400

    fix auth

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { Settings, BarChart3, ChevronLeft, CheckCircle2, AlertCircle, Loader2, S
 
 // --- CONFIGURACIÓN DE FIREBASE ---
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,6 +25,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const appId = 'surveypro-app';
 
 // --- CONSTANTES ---
 const RATING_OPTIONS = [
